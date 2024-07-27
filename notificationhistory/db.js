ad044571e1122b316229f3fbaddc9e54c889cf4c
Matari7 <arimcf10@hotmail.es>
2024-07-26 23:02:31 -0500

    Update notification db and deploy.yml
    Cambiado region2

--- notificationhistory/db.js
+++ notificationhistory/db.js
@@ -1,7 +1,7 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
 
-const REGION = "us-east-2"; // Change to your AWS region
+const REGION = process.env.AWS_REGION2; // Change to your AWS region
 const client = new DynamoDBClient({ region: REGION });
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
