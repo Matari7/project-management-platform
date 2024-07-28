4fcc867dbcdcf275907d0f6a0b14435817b77222
Matari7 <arimcf10@hotmail.es>
2024-07-27 20:37:17 -0500

    Update pipeline

--- createproject/db.js
+++ createproject/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const mysqlUsersPool = mysql.createPool({
-  host: 'mysql-cristiancaiza.alwaysdata.net',
-  user: '357693_cris',
-  password: 'mysql123',
-  database: 'cristiancaiza_users',
-  port: 3306,
+  host: process.env.USER_DB_HOST,
+  user: process.env.USER_DB_USER,
+  password: process.env.USER_DB_PASSWORD,
+  database: process.env.USER_DB_NAME,
+  port: process.env.USER_DB_PORT,
 });
 
 const mysqlProjectsPool = mysql.createPool({
